9e8580aa867e7e6c7cb5f19a107109655ca042ab
Teriq Perry <Teriq_Perry@apples-MacBook-Pro.local>
2016-04-04 20:35:23 -0500

    added js functionality

--- quiz.js
+++ quiz.js
@@ -3,33 +3,36 @@ var CarLot = (function(showCar) {
   var displayCar = document.getElementById("car-view");
   var carChoice = [];
 
-  
-  showCar.carObj = function(carList) {
-    for (var list in carList) {
-      carChoice.push(carList[list]);
-    }
-  }
-
   showCar.populatePage = function(cars) {
-    // showCar.carObj(inventory)
     var inventory = cars.cars
     console.log("Hmmmm", inventory)
     var carString = "";
     for (var i = 0; i < inventory.length; i++) {
       var inventoryList = inventory[i]
-      carString += `<h2>${inventoryList.make}</h2>`;
+      carString += `<div class="col-xs-4"><h2>${inventoryList.make}</h2>`;
       carString += `<h3>${inventoryList.model}</h3>`;
       carString += `<h4>${inventoryList.color}</h4>`;
       carString += `<h4>${inventoryList.year}</h4>`;
       carString += `<p>${inventoryList.description}</p>`;
-      carString += `<footer>Price: ${inventoryList.price}</footer>`;
-
-    	// cars += "<h3>" + inventoryList.color + inventoryList.make + "</h3> <br />" + "<h5>" + inventoryList.model + "</h5> <br />" + "<div>" + inventoryList.year + "</div> <br />" + "<div>" + inventoryList.description + "</div> <br />" + "<footer>" + "Price: " + inventoryList.price + "</footer>"
+      carString += `<footer>Price: ${inventoryList.price}</footer></div>`;
     };
     
     displayCar.innerHTML += carString;
   }
 
+  showCar.clearInput = function() {
+    document.getElementById("input").value = "";
+    document.getElementById("input").focus();
+  }
+
+
+  showCar.styleSheet = function(inventory) {
+    console.log("class", inventory)
+    for (var i = 0; i < inventory.length; i++) {
+      document.getElementByClassName("col-xs-4").style.borderColor = cars[i].color;
+    };
+  }
+
   return showCar;
 
-}(CarLot))
+}(CarLot))  
